0790162eb45767926578cef450dbe0ddac125694
Rishav <96347910+Rishav-Singha-Deo@users.noreply.github.com>
2021-12-25 21:59:44 +0530

    Update String Stuff.py

--- String Stuff.py
+++ String Stuff.py
@@ -5,16 +5,11 @@ print("(c) The first character of the string :",s[0])
 print("(d) The first four characters of the string :",s[0:4])
 print("(e) The last two characters of the string :",s[-2:])
 print("(f) The string in reversed form :",s[::-1])
-print(("(g) The ninth character of the string :", s[9-1]) if len(s)>=9 else "(g) The string is too short for a ninth character.")
+print("(g) The ninth character of the string :", s[9-1] if len(s)>=9
+else "(g) The string is too short for a ninth character.")
 print("(h) The string with its first and last characters removed :",s[1:-1])
 print("(i) The string in all the letters in capital form :",s.upper())
-st1=""
-for i in s:
-    if i=="i":
-        st1+="I"
-    else:
-        st1+=i
-print("(j) The string with every 'i' replaced with an 'I' :",st1)
+print("(j) The string with every 'i' replaced with an 'I' :",s.replace("i","I"))
 st2=""
 for i in s:
     c=ord(i)
