9d7ad8350597319975686af0011891da22d52834
Rishav <96347910+Rishav-Singha-Deo@users.noreply.github.com>
2021-12-25 22:00:32 +0530

    Update Count Words.py

--- Count Words.py
+++ Count Words.py
@@ -2,16 +2,7 @@ s=input("Enter any string : ")
 s=s.split(" ")
 c=0
 for i in s:
-    i=i.replace(".","")
-    i=i.replace(",","")
-    i=i.replace("?","")
-    i=i.replace("!","")
-    i=i.replace(":","")
-    i=i.replace(";","")
-    i=i.replace('"',"")
-    i=i.replace("'","")
-    i=i.replace("-","")
-    if i.isalpha()==True:
+    if i[0].isalpha():
         c+=1
 if c==0:
     print("There are no words in the string.")
