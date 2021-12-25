dd30959bb20ca6f94f201e9e10b359d49b6ec898
Rishav <96347910+Rishav-Singha-Deo@users.noreply.github.com>
2021-12-25 22:04:59 +0530

    Update Vowel Sensor.py

--- Vowel Sensor.py
+++ Vowel Sensor.py
@@ -1,11 +1,7 @@
 s=input("Enter any string : ")
-v=["a","e","i","o","u"]
 c=0
 for i in s:
-    if i in v:
-        c=1
-        break
-if c==1:
-    print("The string contains vowels")
-else:
-    print("The string doesn't contain any vowels.")
+    if i in "aeiouAEIOU":
+        print("The string contains vowels")
+        exit()
+print("The string doesn't contain any vowels.")
